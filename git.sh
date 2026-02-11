711128057be17f841db4f47452bd2e0e218b78fb
Jianping Zhou <drzhou68@gmail.com>
2026-02-11 04:43:58 -0800

    Initial commit: Spectroscopic sonification for molecular AI
    - NMR, CD, IR, MS spectroscopy to audio conversion
    - Wav2Vec 2.0 embedding extraction
    - Real dataset examples (BMRB, PCDDB)
    - Applications: protein function, drug binding, QC

--- git.sh
+++
@@ -1,7 +0,0 @@
-#cd bioai/molecular_ai_project
-git init
-git add .
-git commit -m "Initial commit: Spectroscopic sonification"
-git branch -M main
-git remote add origin https://github.com/molaudionet/spectrosonic.git
-git push -u origin main
